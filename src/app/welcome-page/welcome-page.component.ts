aff3550418f3d8ba3cf955c2e6c7b842993a3761
Melisa Gülşan Yılmaz <melisagulsanyilmaz@eskisehir.edu.tr>
2022-10-17 01:08:50 +0300

    phone input added

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -93,7 +93,7 @@ export class WelcomePage {
     ]),
     password: new FormControl("", [
       Validators.required,
-      // Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+      Validators.pattern("^(?=.*[a-z])(?=.*[0-9]).{6,20}$"),
     ]),
     name: new FormControl("", Validators.required),
   });
